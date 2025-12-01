fb4fcece3873ed7e86863ae69dee7752dbab8b9c
Ryan Fitz-Gerald <ztif33@gmail.com>
2025-12-01 15:32:15 -0500

    Added PID configuration object

---
+++ src/main/java/frc/robot/util/PIDConfig.java
@@ -0,0 +1,30 @@
+package frc.robot.util;
+
+import edu.wpi.first.math.controller.PIDController;
+
+public class PIDConfig {
+    // TODO Implement using units library
+    public double kP;
+    public double kI;
+    public double kD;
+
+    /**
+     * Constructor
+     * @param kP    Proportional gain
+     * @param kI    Integral gain
+     * @param kD    Derivative gain
+     */
+    public PIDConfig(double kP, double kI, double kD) {
+        this.kP = kP;
+        this.kI = kI;
+        this.kD = kD;
+    }
+
+    /**
+     * Generates a PID controller object
+     * @return  new PID controller object
+     */
+    public PIDController getPIDController(){
+        return new PIDController(kP, kI, kD);
+    }
+}
